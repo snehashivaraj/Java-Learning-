f1b4b4b74d46b0e6f80d32da946aefda1893b19d
Sneha S <snehakshivarajgowda@gmail.com>
2025-02-17 11:41:35 +0530

    Added Upcasting

---
+++ UpcastingExample.java
@@ -0,0 +1,22 @@
+//upcasting changing to class type 
+
+public class UpcastingExample{	//using builtin class
+
+	public static void changingtype(){	//toparent
+
+		java.lang.Byte by = 88;
+		java.io.Serializable se = by;    //upcasting 
+		System.out.println(se);
+	}
+
+}
+
+class UpcastingExampleMain{
+
+	public static void main(String... fgiodfgj){
+
+		UpcastingExample.changingtype();
+
+
+	}
+}
