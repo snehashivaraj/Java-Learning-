c56864d938821e2335a435c6e993a2c8e7cc0e90
Sneha S <snehakshivarajgowda@gmail.com>
2025-02-18 08:52:27 +0530

    removed old files

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-// first program in github
-public class HelloWorld{
-  public static void main(String[] args){
-    System.out.println("Hello GitHub!");
-  }
-}
---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+// first program in github
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello GitHub!");
+  }
+}
